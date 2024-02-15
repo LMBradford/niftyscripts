221b47504645ace547d079d39ceec61921e7c8ae
laurenbradford <lrn.bradford@gmail.com>
2024-02-15 14:11:43 -0500

    Add command line arguments for outfile and pattern prefix

--- count_fastqreads_rough.py
+++ count_fastqreads_rough.py
@@ -2,15 +2,31 @@ import os
 import glob
 import subprocess
 import pandas as pd
+import argparse
 
-def count_fastq_reads(directory=None, output_file='readcount_rough.csv'):
+# Create the parser
+parser = argparse.ArgumentParser(description='Count reads in fastq files.')
+
+# Add the arguments
+parser.add_argument('-o','--OutputFile',
+                    metavar='output_file',
+                    type=str,
+                    help='the name of the output file')
+parser.add_argument('-p','--prefix', 
+                    type=str, 
+                    help='prefix pattern of the input files. Optional')                    
+
+# Parse the arguments
+args = parser.parse_args()
+
+def count_fastq_reads(directory=None, output_file=args.OutputFile):
     # If no directory is specified, use the current working directory
     if directory is None:
         directory = os.getcwd()
 
-    # Get all fastq.gz files in the directory
-    files = glob.glob('*.fastq.gz')
-
+    # Get all fastq.gz files matching a given pattern
+    files = glob.glob(f'{args.prefix}*.fastq.gz')
+    print(files)
     # List to store the results
     data = []
 
@@ -28,5 +44,6 @@ def count_fastq_reads(directory=None, output_file='readcount_rough.csv'):
     # Output the DataFrame to a CSV file
     df.to_csv(output_file, index=False)
     print(df)
+
 # Usage
-count_fastq_reads('readcount_rough.csv')
+count_fastq_reads()
